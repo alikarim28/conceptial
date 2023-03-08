920709c7451cdc02316e139da6fde82ad5851e18
redawehbe <redawehbe18@gmail.com>
2023-03-08 23:29:36 +0200

    nf

--- sproduct.php
+++ sproduct.php
@@ -10,48 +10,50 @@
 	<link rel = "icon" href ="images/icon-1.png" type = "image/x-icon">
 </head>
 <body>
-<?php require"components/header.php";
+<?php require "components/header.php";
 echo "</div> </div>"; ?>
-    <div id="prodetails" class="section-p1">
-        <div class="single-pro-image">
-            <img src="images2/product-1.jpg" width="100%" id="MainImg">
+<?php require "functions/common_functions.php";
+    view_product();
+?>
+        <!-- <div id="prodetails" class="section-p1">
+            <div class="single-pro-image">
+                <img src="images2/product-1.jpg" width="100%" id="MainImg">
 
-            <div class="small-img-group">
-                <div class="small-img-col">
-                    <img src="images2/product-3.jpg" width="100%" class="small-img">
+                <div class="small-img-group">
+                    <div class="small-img-col">
+                        <img src="images2/product-3.jpg" width="100%" class="small-img">
+                    </div>
+                    <div class="small-img-col">
+                        <img src="images2/product-4.jpg" width="100%" class="small-img">
+                    </div>
+                    <div class="small-img-col">
+                        <img src="images2/product-5.jpg" width="100%" class="small-img">
+                    </div>
+                    <div class="small-img-col">
+                        <img src="images2/product-6.jpg" width="100%" class="small-img">
+                    </div>
                 </div>
-                <div class="small-img-col">
-                    <img src="images2/product-4.jpg" width="100%" class="small-img">
-                </div>
-                <div class="small-img-col">
-                    <img src="images2/product-5.jpg" width="100%" class="small-img">
-                </div>
-                <div class="small-img-col">
-                    <img src="images2/product-6.jpg" width="100%" class="small-img">
-                </div>
+
             </div>
-
-        </div>
-        <div class="single-pro-details">
-            <h6>Home / T-shirt</h6>
-            <h4>Men's Fashion T Shirt</h4>
-            <h2> 20.00$</h2>
-            <select>
-                <option> Select Size</option>
-                <option> XL</option>
-                <option> XXL</option>
-                <option> Small</option>
-                <option> Large</option>
-            </select>    
-            <input type="number" value="1">
-            <a href="" class="btn">Add To Cart</a><br>
-            <h4>Product Details</h4>
-            <span>The green to Wear 2.0 Standard aims to minimize the environmental impact of textile manufacturing.
-                 To that end, we have developed Inditex's The List Programme,
-                 which helps guarantee both that production processes are clean and that pur garments are safe and healthy.
-            </span>
-        </div>
-    </div>
+            <div class="single-pro-details">
+                <h6>Home / T-shirt</h6>
+                <h4>Men's Fashion T Shirt</h4>
+                <h2> 20.00$</h2>
+                <select>
+                    <option> Select Size</option>
+                    <option> Large</option>
+                    <option> Small</option>
+                    <option> Large</option>
+                </select>    
+                <input type="number" value="1">
+                <a href="" class="btn">Add To Cart</a><br>
+                <h4>Product Details</h4>
+                <span>The green to Wear 2.0 Standard aims to minimize the environmental impact of textile manufacturing.
+                    To that end, we have developed Inditex's The List Programme,
+                    which helps guarantee both that production processes are clean and that pur garments are safe and healthy.
+                </span>
+            </div>
+        </div> -->
 	<?php
 	require "components/offer.php";
 	require "components/brand.php";
