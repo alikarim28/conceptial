b997db2952a001c048858ba93b254f000e4ab754
alikarim28 <73882383+alikarim28@users.noreply.github.com>
2023-04-04 10:31:37 +0000

    edit products

--- admin_area/edit_products.php
+++ admin_area/edit_products.php
@@ -1,16 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div class="container mt-5">
-        <h1 class="text-center">
-            Edit products
-        </h1>
+<?php
+    if(isset($_GET['edit_products'])){
+        $edit_id = $_GET['edit_products'];
+    }
+?>
+  
+  <div class="container mt-5">
         <form action="" method="post" enctype="multipart/form-data">
             <div class="form-outline w-50 m-auto">
                 <label for="product_title" class="form-label">Product title</label>
@@ -71,5 +65,3 @@
             </div>
         </form>
     </div>
-</body>
-</html>
