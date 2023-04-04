98dd2ec4f24b79f3a4328485a02c33e7d89a1f51
Aly012 <95579937+Aly012@users.noreply.github.com>
2023-04-05 01:26:22 +0300

    delete item

--- admin_area/delete_products.php
+++ admin_area/delete_products.php
@@ -1,4 +1,15 @@
 <?php
-    
+    if(isset($_GET['delete_products'])){
+        $delete_id = $_GET['delete_products'];
+
+        $delete_qery = 'DELETE from `product`  where product_id = $delete_id';
+        $result_product =  mysqli_query($con,$delete_qery);
+        if($result_product){
+            echo "<script>alert('product deleted successfully')</script>";
+            echo "<script>window.open('./index.php','_self')</script>";
+        }else{
+            die("error");
+        }
+    }
 
 ?>
--- admin_area/index.php
+++ admin_area/index.php
@@ -69,6 +69,9 @@
     if(isset($_GET['edit_products'])){
         include('edit_products.php');
     }
+    if(isset($_GET['delete_products'])){
+        include('delete_products.php');
+    }
     ?>
 </div>
 </body>
